895e2ac475fe138c4ce3b9b1e148e182a13a1e86
Aaron McAdam <aaron@aaronmcadam.com>
2021-11-23 18:24:24 +0000

    message bg

--- src/components/ceo-message.tsx
+++ src/components/ceo-message.tsx
@@ -4,42 +4,44 @@ import { QuotesIcon } from "./quotes-icon";
 
 export function CeoMessage() {
   return (
-    <HStack spacing={10} maxW="6xl" mx="auto" px={6} pos="relative" mt={16}>
-      <Pattern
-        color="gray.200"
-        h="404px"
-        pos="absolute"
-        left={0}
-        transform="auto"
-        translateX={-60}
-      />
-      <Avatar size="3xl" src="/danny.png" />
-      <Box pos="relative" ml={10}>
-        <QuotesIcon
-          boxSize={36}
+    <Box overflowY="hidden">
+      <HStack spacing={10} maxW="6xl" mx="auto" pos="relative" py={20}>
+        <Pattern
+          color="gray.200"
+          h="404px"
           pos="absolute"
-          top={0}
           left={0}
           transform="auto"
-          translateX={-8}
-          translateY={-24}
-          color="purple.200"
-          opacity={0.5}
+          translateX={-60}
         />
-        <Stack as="blockquote" spacing={8} pos="relative">
-          <Text fontWeight="medium" fontSize="xl" maxW="2xl">
-            Our priority is the customer, not the provider. People are often
-            amazed at the saving that they can make—we’ve helped business across
-            many sectors save thousands on their electricity bills.
-          </Text>
-          <Stack spacing={0}>
-            <Text fontWeight="medium">Daniel McAdam</Text>
-            <Text fontWeight="medium" color="purple.600">
-              CEO, First Choice Utility Options
+        <Avatar size="3xl" src="/danny.png" />
+        <Box pos="relative" ml={10}>
+          <QuotesIcon
+            boxSize={36}
+            pos="absolute"
+            top={0}
+            left={0}
+            transform="auto"
+            translateX={-8}
+            translateY={-24}
+            color="purple.200"
+            opacity={0.5}
+          />
+          <Stack as="blockquote" spacing={8} pos="relative">
+            <Text fontWeight="medium" fontSize="xl" maxW="2xl">
+              Our priority is the customer, not the provider. People are often
+              amazed at the saving that they can make—we’ve helped business
+              across many sectors save thousands on their electricity bills.
             </Text>
+            <Stack spacing={0}>
+              <Text fontWeight="medium">Daniel McAdam</Text>
+              <Text fontWeight="medium" color="purple.600">
+                CEO, First Choice Utility Options
+              </Text>
+            </Stack>
           </Stack>
-        </Stack>
-      </Box>
-    </HStack>
+        </Box>
+      </HStack>
+    </Box>
   );
 }
