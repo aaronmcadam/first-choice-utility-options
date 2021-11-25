483b682cf876a3eb6095e6291dfcdd57b45f1646
Aaron McAdam <aaron@aaronmcadam.com>
2021-11-25 18:41:35 +0000

    fix(hero): tweak logo size

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -169,7 +169,7 @@ export function Hero() {
   return (
     <Box maxW="7xl" mx="auto" px={{ base: 4, sm: 6 }}>
       <Flex justify="space-between">
-        <FirstChoice h={{ base: 5, sm: 8 }} />
+        <FirstChoice h={{ base: 6, sm: 8 }} />
         {isDesktop ? (
           <Button as="a" href="tel:00353872470718">
             Call us
