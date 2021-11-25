3d3c76d1b2f9518c060b5946d17a55f8215b7f4d
Aaron McAdam <aaron@aaronmcadam.com>
2021-11-25 14:44:26 +0000

    refactor(Hero): prepare for more changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,6 +15,142 @@ import { useLayout } from "../hooks/useLayout";
 import { FirstChoice } from "./logos/first-choice";
 import { PlayIcon } from "./play-icon";
 
+function BaseLayout() {
+  return (
+    <Stack spacing={12} mt={8}>
+      <Stack align="flex-start" spacing={5}>
+        <Heading as="h1" fontSize="3xl" fontWeight="extrabold">
+          <Box
+            as="mark"
+            bgGradient="linear(to-r, #FC19AD, #7F23F7, #14AFFC)"
+            bgClip="text"
+            display="block"
+          >
+            Save more than 25%
+          </Box>{" "}
+          off your energy bill
+        </Heading>
+        <Text color="gray.500" mt="5" maxW="2xl">
+          We ensure you get the best deal by keeping on top of the ever-changing
+          tariffs from all the energy providers in Ireland.
+        </Text>
+        <Button
+          as="a"
+          href="tel:00353872470718"
+          variant="primary"
+          size="lg"
+          w="full"
+        >
+          Get a quote
+        </Button>
+      </Stack>
+      <Box pos="relative" overflow="hidden" rounded="lg" shadow="lg">
+        <chakra.button
+          as="a"
+          href="https://youtu.be/UmzX6VxRshw"
+          target="_blank"
+          rel="noopener noreferrer"
+          pos="relative"
+          display="block"
+        >
+          <VisuallyHidden>Watch our video to learn more</VisuallyHidden>
+          <Image
+            alt="Watch our video to learn more"
+            src="/video-thumbnail.png"
+            w="full"
+          />
+          <Flex
+            aria-hidden="true"
+            align="center"
+            justify="center"
+            pos="absolute"
+            top={0}
+            right={0}
+            bottom={0}
+            left={0}
+            w="full"
+            h="full"
+          >
+            <PlayIcon color="purple.500" boxSize={20} />
+          </Flex>
+        </chakra.button>
+      </Box>
+    </Stack>
+  );
+}
+
+function LargeLayout() {
+  return (
+    <SimpleGrid columns={12} spacing={8} mt={32}>
+      <Stack gridColumn="span 6" align="flex-start" spacing={5}>
+        <Heading
+          as="h1"
+          fontSize={{ base: "3xl", sm: "6xl" }}
+          fontWeight="extrabold"
+        >
+          <Box
+            as="mark"
+            bgGradient="linear(to-r, #FC19AD, #7F23F7, #14AFFC)"
+            bgClip="text"
+            display="block"
+          >
+            Save more than 25%
+          </Box>{" "}
+          off your energy bill
+        </Heading>
+        <Text color="gray.500" mt="5" fontSize="xl" maxW="2xl">
+          We ensure you get the best deal by keeping on top of the ever-changing
+          tariffs from all the energy providers in Ireland.
+        </Text>
+        <Button
+          as="a"
+          href="tel:00353872470718"
+          variant="primary"
+          size="lg"
+          w={{ base: "full", sm: "auto" }}
+        >
+          Get a quote
+        </Button>
+      </Stack>
+      <Flex gridColumn="span 6" align="center">
+        <Box rounded="lg" shadow="lg" mx="auto" w="full" maxW="md">
+          <chakra.button
+            as="a"
+            href="https://youtu.be/UmzX6VxRshw"
+            target="_blank"
+            rel="noopener noreferrer"
+            pos="relative"
+            display="block"
+            rounded="lg"
+            overflow="hidden"
+          >
+            <VisuallyHidden>Watch our video to learn more</VisuallyHidden>
+            <Image
+              alt="Watch our video to learn more"
+              src="/video-thumbnail.png"
+              w="full"
+            />
+            <Flex
+              aria-hidden="true"
+              align="center"
+              justify="center"
+              pos="absolute"
+              top={0}
+              right={0}
+              bottom={0}
+              left={0}
+              w="full"
+              h="full"
+            >
+              <PlayIcon color="purple.500" boxSize={20} />
+            </Flex>
+          </chakra.button>
+        </Box>
+      </Flex>
+    </SimpleGrid>
+  );
+}
+
 export function Hero() {
   const { isMobile, isDesktop } = useLayout();
 
@@ -28,135 +164,8 @@ export function Hero() {
           </Button>
         ) : null}
       </Flex>
-      {isMobile ? (
-        <Stack spacing={12} mt={8}>
-          <Stack align="flex-start" spacing={5}>
-            <Heading as="h1" fontSize="3xl" fontWeight="extrabold">
-              <Box
-                as="mark"
-                bgGradient="linear(to-r, #FC19AD, #7F23F7, #14AFFC)"
-                bgClip="text"
-                display="block"
-              >
-                Save more than 25%
-              </Box>{" "}
-              off your energy bill
-            </Heading>
-            <Text color="gray.500" mt="5" maxW="2xl">
-              We ensure you get the best deal by keeping on top of the
-              ever-changing tariffs from all the energy providers in Ireland.
-            </Text>
-            <Button
-              as="a"
-              href="tel:00353872470718"
-              variant="primary"
-              size="lg"
-              w="full"
-            >
-              Get a quote
-            </Button>
-          </Stack>
-          <Box pos="relative" overflow="hidden" rounded="lg" shadow="lg">
-            <chakra.button
-              as="a"
-              href="https://youtu.be/UmzX6VxRshw"
-              target="_blank"
-              rel="noopener noreferrer"
-              pos="relative"
-              display="block"
-            >
-              <VisuallyHidden>Watch our video to learn more</VisuallyHidden>
-              <Image
-                alt="Watch our video to learn more"
-                src="/video-thumbnail.png"
-                w="full"
-              />
-              <Flex
-                aria-hidden="true"
-                align="center"
-                justify="center"
-                pos="absolute"
-                top={0}
-                right={0}
-                bottom={0}
-                left={0}
-                w="full"
-                h="full"
-              >
-                <PlayIcon color="purple.500" boxSize={20} />
-              </Flex>
-            </chakra.button>
-          </Box>
-        </Stack>
-      ) : (
-        <SimpleGrid columns={12} spacing={8} mt={32}>
-          <Stack gridColumn="span 6" align="flex-start" spacing={5}>
-            <Heading
-              as="h1"
-              fontSize={{ base: "3xl", sm: "6xl" }}
-              fontWeight="extrabold"
-            >
-              <Box
-                as="mark"
-                bgGradient="linear(to-r, #FC19AD, #7F23F7, #14AFFC)"
-                bgClip="text"
-                display="block"
-              >
-                Save more than 25%
-              </Box>{" "}
-              off your energy bill
-            </Heading>
-            <Text color="gray.500" mt="5" fontSize="xl" maxW="2xl">
-              We ensure you get the best deal by keeping on top of the
-              ever-changing tariffs from all the energy providers in Ireland.
-            </Text>
-            <Button
-              as="a"
-              href="tel:00353872470718"
-              variant="primary"
-              size="lg"
-              w={{ base: "full", sm: "auto" }}
-            >
-              Get a quote
-            </Button>
-          </Stack>
-          <Flex gridColumn="span 6" align="center">
-            <Box rounded="lg" shadow="lg" mx="auto" w="full" maxW="md">
-              <chakra.button
-                as="a"
-                href="https://youtu.be/UmzX6VxRshw"
-                target="_blank"
-                rel="noopener noreferrer"
-                pos="relative"
-                display="block"
-                rounded="lg"
-                overflow="hidden"
-              >
-                <VisuallyHidden>Watch our video to learn more</VisuallyHidden>
-                <Image
-                  alt="Watch our video to learn more"
-                  src="/video-thumbnail.png"
-                  w="full"
-                />
-                <Flex
-                  aria-hidden="true"
-                  align="center"
-                  justify="center"
-                  pos="absolute"
-                  top={0}
-                  right={0}
-                  bottom={0}
-                  left={0}
-                  w="full"
-                  h="full"
-                >
-                  <PlayIcon color="purple.500" boxSize={20} />
-                </Flex>
-              </chakra.button>
-            </Box>
-          </Flex>
-        </SimpleGrid>
-      )}
+      {isMobile ? <BaseLayout /> : null}
+      {isDesktop ? <LargeLayout /> : null}
     </Box>
   );
 }
